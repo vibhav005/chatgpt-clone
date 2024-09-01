42ac1fd8983f0a9c9790511a418577ace7b86126
Vibhav <veebhavjoshi1@gmail.com>
2024-09-01 23:36:19 +0530

    feat - Add navbar and dashboard to the layouts and clerk authentication

---
+++ src/layouts/root/RootLayout.jsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from "react-router-dom"
+import "./rootLayout.css"
+
+const RootLayout = () => {
+    return (
+        <div className="rootLayout">
+            <header>
+                <Link to="/" className="logo">
+                    <img src="/logo.png" alt="" />
+                    <span>GOKU AI</span>
+                </Link>
+                <div className="user">USER</div>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
+
+export default RootLayout
--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import DashboardLayout from './layouts/dashboard/DashboardLayout'
+import RootLayout from './layouts/root/RootLayout'
 import ChatPage from "./routes/chat/ChatPage"
 import DashboardPage from "./routes/dashboard/DashboardPage"
 import HomePage from "./routes/home/HomePage"
 
+
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/dashboard",
+    element: <RootLayout />,
     children: [
-      { path: "/dashboard", element: <DashboardPage /> },
-      { path: "/dashboard/chats/:id", element: <ChatPage /> }
-    ],
-  },
+      {
+        path: "/",
+        element: <HomePage />
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />
+          }
+        ]
+      }
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
