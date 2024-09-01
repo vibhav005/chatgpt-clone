ab88fe3a9b72db1ee33146a04672b7f5decb3541
Vibhav <veebhavjoshi1@gmail.com>
2024-09-01 22:40:03 +0530

    Add prettier and some first setup of css and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import ChatPage from "./routes/chat/ChatPage"
+import DashboardPage from "./routes/dashboard/DashboardPage"
+import HomePage from "./routes/home/HomePage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/dashboard",
+    children: [
+      { path: "/dashboard", element: <DashboardPage /> },
+      { path: "/dashboard/chats/:id", element: <ChatPage /> }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import "./homePage.css"
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>HomePage</div>
+    )
+}
+
+export default HomePage
