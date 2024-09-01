01ae51731e4cd58eff9c8ef614b0f33aeb38926c
Vibhav <veebhavjoshi1@gmail.com>
2024-09-02 00:48:56 +0530

    feat - add homepage bot animation and css

--- src/routes/home/HomePage.jsx
+++ src/routes/home/HomePage.jsx
@@ -4,7 +4,21 @@ import "./homePage.css"
 const HomePage = () => {
     return (
         <div className='homepage'>
-            <Link to="/dashboard">Dashboard</Link>
+            <img src="/orbital.png" alt="" className="orbital" />
+            <div className="left">
+                <h1>GOKU AI</h1>
+                <h2>Supercharge your prdouctivity and creativity.</h2>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum impedit modi facere repellendus illum voluptatibus similique.</h3>
+                <Link to="/dashboard">Get Started</Link>
+            </div>
+            <div className="right">
+                <div className="imgContainer">
+                    <div className="bgContainer">
+                        <div className="bg"></div>
+                    </div>
+                    <img src="/bot.png" alt="" className="bot" />
+                </div>
+            </div>
         </div>
     )
 }
